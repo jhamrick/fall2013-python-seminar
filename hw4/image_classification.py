f09e5104fea654543494f684594afe05d2614137
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-10-02 14:18:57 -0700

    Reproducible randomness; run classification on verification set

--- hw4/image_classification.py
+++ hw4/image_classification.py
@@ -3,6 +3,13 @@ from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+import pickle
+from glob import glob
+import sys
+import os
+from itertools import izip
+
+from image_processing import load_and_extract
 
 
 def split_data(dataset, ftrain=0.9, rso=None):
@@ -26,10 +33,16 @@ def split_data(dataset, ftrain=0.9, rso=None):
     return X_train, X_test, Y_train, Y_test
 
 
-def train_classifier(X, Y, rso=None):
+def train_classifier(X, Y, save=False, rso=None):
     # train a random forest classifier on the data
     clf = RandomForestClassifier(random_state=rso)
     clf.fit(X, Y)
+
+    # optionally save the pickled classifier to disk
+    if save:
+        with open("image_classifier.pkl", "w") as fh:
+            pickle.dump(clf, fh)
+
     return clf
 
 
@@ -51,3 +64,42 @@ def display_confusion_matrix(Y_test, Y_pred, categories):
 
     fig.set_figwidth(6)
     fig.set_figheight(6)
+
+
+def run_final_classifier(directory):
+
+    # get the list of images
+    images = glob("%s/*.jpg" % directory)
+    # get the list of categories
+    categories = np.load("./image_categories.npy")
+
+    # compute feature matrix
+    features = load_and_extract(images)
+
+    # load the classifier
+    with open("./image_classifier.pkl", "r") as fh:
+        clf = pickle.load(fh)
+
+    # make predictions
+    Y_pred = clf.predict(features)
+
+    # save the results to file
+    results_file = "./results.txt"
+    with open(results_file, "w") as fh:
+        def write(msg):
+            sys.stdout.write(msg)
+            fh.write(msg)
+
+        write("filename\tpredicted_class\n")
+        write("-"*30 + "\n")
+
+        for image, pred in izip(images, Y_pred):
+            filename = os.path.split(image)[1]
+            line = "%s\t%s\n" % (filename, categories[pred])
+            write(line)
+
+    print "Results saved to '%s'." % results_file
+
+
+if __name__ == "__main__":
+    run_final_classifier(sys.argv[1])
