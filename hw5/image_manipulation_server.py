bb6ccc65f54f33cce26f1b0eaa38e534a011902b
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-10-09 18:13:14 -0700

    Rename server.py; print method help

--- hw5/image_manipulation_server.py
+++ hw5/image_manipulation_server.py
@@ -1,8 +1,55 @@
 import os
+import numpy as np
+import StringIO
+import xmlrpclib
 
 from PIL import Image
 from SimpleXMLRPCServer import SimpleXMLRPCServer
-from xmlimage import encode, decode
+
+
+def encode(image):
+    """Encode an image to XML-RPC base-64 encoding.
+
+    Parameters
+    ----------
+    image : np.ndarray
+
+    Returns
+    -------
+    xmlrpclib.Binary instance
+
+    """
+    # save the image into a string buffer
+    strio = StringIO.StringIO()
+    Image.fromarray(image).save(strio, format="jpeg")
+    # read out the string from the buffer
+    strio.seek(0)
+    imstr = strio.read()
+    strio.close()
+    # create a binary object from the string
+    return xmlrpclib.Binary(imstr)
+
+
+def decode(binary):
+    """Decode an image from XML-RPC base-64 encoding to an array.
+
+    Parameters
+    ----------
+    binary : xmlrpclib.Binary instance
+
+    Returns
+    -------
+    numpy.ndarray
+
+    """
+    # create a string buffer from the string (binary data)
+    strio = StringIO.StringIO(binary.data)
+    # create a PIL image from that string and then convert it to a
+    # numpy array
+    image = Image.open(strio)
+    image = np.asarray(image).copy()
+    strio.close()
+    return image
 
 
 def serializable(func):
@@ -19,7 +66,17 @@ def serializable(func):
 
     # ensure that the decorated function has the same name and
     # docstring as the original function
-    code_image.__doc__ = func.__doc__
+    code_image.__doc__ = func.__doc__ + """
+
+    Parameters
+    ----------
+    image : xmlrpclib.Binary object
+
+    Returns
+    -------
+    xmlrpclib.Binary object
+
+    """
     code_image.__name__ = func.__name__
     return code_image
 
