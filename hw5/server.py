6dd50b339d9fb2fbe5578a0a3839488be73e51e2
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-10-09 17:44:51 -0700

    Save images on client and server side

--- hw5/server.py
+++ hw5/server.py
@@ -1,5 +1,7 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from xmlimage import encode, decode
+from PIL import Image
+import os
 
 
 def serializable(func):
@@ -12,25 +14,46 @@ def serializable(func):
     return code_image
 
 
+def save_images(func):
+    def save(self, image):
+        name = func.__name__
+        new_image = func(self, image)
+        if not os.path.exists("server_images"):
+            os.makedirs("server_images")
+        Image.fromarray(image).save(
+            "server_images/%s_in.jpg" % name)
+        Image.fromarray(new_image).save(
+            "server_images/%s_out.jpg" % name)
+        return new_image
+    save.__doc__ = func.__doc__
+    save.__name__ = func.__name__
+    return save
+
+
 class ImageManipulations(object):
 
     @serializable
+    @save_images
     def invert(self, image):
         return 255 - image
 
     @serializable
+    @save_images
     def flip_vertical(self, image):
         return image[::-1]
 
     @serializable
+    @save_images
     def flip_horizontal(self, image):
         return image[:, ::-1]
 
     @serializable
+    @save_images
     def rotate_counterclockwise(self, image):
         return image.swapaxes(0, 1)[::-1]
 
     @serializable
+    @save_images
     def rotate_clockwise(self, image):
         return image.swapaxes(0, 1)[:, ::-1]
 
