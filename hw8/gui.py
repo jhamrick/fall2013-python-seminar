1216931479d91bebfac86de7eaa274ba52724de3
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-10-30 16:38:59 -0700

    Set up rough GUI skeleton

---
+++ hw8/gui.py
@@ -0,0 +1,113 @@
+"""Homework 8: GUIs
+
+Author: Jessica Hamrick <jhamrick@berkeley.edu>
+
+Instructions
+------------
+
+Using Traits and TraitsUI create a GUI application that consists of
+a search box, a text display, an image display, and a series of
+buttons. The application will accept search strings in the search box
+and then run an internet image search. The first returned image from
+the search will be downloaded, the image url displayed in the text
+display field, and the actual image displayed in the image display
+field.
+
+The buttons will provide the user interface to run the image search as
+well as perform manipulations on the image currently stored in the
+display (for example, blurring or rotation). Provide at least three
+unique (and interesting) image manipulation functions.
+
+Tips: Feel free to use an image search (or, include support for
+multiple different searches). Yahoo has a convenient search
+module. There is also freedom in how you display the image, but
+matplotlib is recommended.
+
+"""
+
+try:
+    import enthought.traits.api as tr
+    import enthought.traits.ui.api as ui
+except:
+    import traits.api as tr
+    import traitsui.api as ui
+
+
+class ImageDisplay(tr.HasTraits):
+    pass
+
+
+class Container(tr.HasTraits):
+    tags = tr.Str
+    url = tr.Str
+    display = tr.Instance(ImageDisplay)
+
+    refresh = tr.Button
+    blur = tr.Button
+    sharpen = tr.Button
+    smooth = tr.Button
+    edge = tr.Button
+    contrast = tr.Button
+    color = tr.Button
+    decolor = tr.Button
+    brighten = tr.Button
+    darken = tr.Button
+
+    view = ui.View(
+        ui.Group(
+
+            ui.Group(
+                ui.Item(
+                    'tags',
+                    show_label=True,
+                    label="Query string"),
+                label="Input",
+                show_border=True,
+                springy=True),
+
+            ui.Group(
+                ui.Item(
+                    'url',
+                    style='readonly',
+                    show_label=False),
+                label="Image URL",
+                show_border=True,
+                springy=True),
+
+            ui.Group(
+                ui.Item(
+                    'display',
+                    style='custom',
+                    show_label=False),
+                label="Image Display",
+                show_border=True,
+                springy=True),
+
+            ui.Group(
+                ui.Item('refresh'),
+                ui.Item('blur'),
+                ui.Item('sharpen'),
+                ui.Item('smooth'),
+                ui.Item('edge'),
+                ui.Item('contrast'),
+                ui.Item('color'),
+                ui.Item('decolor'),
+                ui.Item('brighten'),
+                ui.Item('darken'),
+
+                orientation='horizontal',
+                show_labels=False,
+                label="Image Manipulation Options",
+                show_border=True,
+                springy=True),
+
+            show_labels=True,
+            orientation='vertical'
+        ),
+
+        title="Image Search",
+    )
+
+container = Container(
+    display=ImageDisplay())
+container.configure_traits()
